73cd52b890a6d9fa6abe33d0094d0745d2a821c2
Shikha <shikhafadnavis@gmail.com>
2017-09-08 17:34:46 -0700

    Final version of the code. Added recursive function.

--- binary-to-linked.c
+++ binary-to-linked.c
@@ -9,6 +9,9 @@ Date   : 09/08/2017
 
 #include<stdio.h>
 #include<stdlib.h>
+#define ARRSIZE 50
+//This limits the number of elements in binary tree to 50. Please change it accordingly.
+
 
 typedef struct node
 {
@@ -17,6 +20,11 @@ typedef struct node
 	struct node* pRight;
 	
 }node;
+
+node* inorderArr[ARRSIZE];
+
+int arrIndex = 0;
+
 
 node* insertNode(int value)
 {
@@ -29,36 +37,14 @@ node* insertNode(int value)
 
 }
 
-node* findNode(node* root, int value)
-{
-	if(root != NULL)
-	{
-		if(root->data == value)
-		{
-			//printf("%d", root->data);
-			//printf("\nhere!");
-			return root;
-
-		}
-		else
-		{
-			return findNode(root->pLeft, value);
-			return findNode(root->pRight, value);
-	
-		}
-
-	}
-	
-
-
-}
 
 void inorder(node* root)
 {
 	if (root != NULL)
 	{
 		inorder(root->pLeft);
-		printf("\n%d", root->data);
+		inorderArr[arrIndex] = root;
+		arrIndex += 1;
 		inorder(root->pRight);
 
 	}
@@ -69,30 +55,51 @@ void inorder(node* root)
 	} 
 
 }
-/*
-node* insertLeft(node* root, int value)
+
+void convertBinaryToLinked(node* head, int i)
 {
-	node* thisNode = root;
-	bool res = false;
-	if(thisNode->data == value)
+
+	if(i < arrIndex && head != NULL)
 	{
-		return thisNode;
+		if(i == 0)
+		{
+			head->pLeft = NULL;
+			head->pLeft = inorderArr[i+1];
+		}
+		if(i == arrIndex-1)
+		{
+			head->pLeft = inorderArr[i-1];
+			head->pRight = NULL;
+		}
+		else
+		{	
+			head->pLeft = inorderArr[i-1];
+			head->pRight = inorderArr[i+1];
+		}
+
+		convertBinaryToLinked(head->pRight, i+1);
+
 	}
 
 	else
 	{
-		
-
+		return;
 	}
-
+		
 
 }
 
-*/
 
 int main()
 {
-	node* resultNode = NULL;
+	
+	int i = 0;
+	int counter = 0;
+	for(i = 0; i < ARRSIZE; i++)
+	{
+		inorderArr[i] = NULL;
+	}
+
 	node* root = insertNode(1);
 	root->pLeft = insertNode(2);
 	root->pRight = insertNode(3);
@@ -101,21 +108,35 @@ int main()
 	root->pLeft->pLeft->pRight = insertNode(8);
 	root->pRight->pLeft = insertNode(5);
 	root->pRight->pRight = insertNode(6);
-	resultNode = findNode(root, 4);
+	root->pLeft->pLeft->pRight->pLeft = insertNode(10);
+	root->pLeft->pLeft->pRight->pRight = insertNode(11);
 
+	
+
+	// Store pointer to each node in an array in an Inorder fashion
 	inorder(root);
 	
+	// Call the recursive function to convert Binary Tree into a Doubly Linked List
+	convertBinaryToLinked(inorderArr[0], counter);
+	
+	node* head = inorderArr[0];
+	printf("Head Node is : %d\n", head->data);
+	
+	// To view the list in forward direction
+	printf("List contents:\n");
+	while(head!=NULL)
+	{
+		printf("%d\n", head->data);
+		head = head->pRight;
+	}
+
 	
 		
-
 	return 0;
 
 	
-	
-	
-
-
-
 
 }
+
+
 
